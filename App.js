47c7b3fdd624b82ee7d0f743bcd3c5cffb28f02d
Ondrej Bures <o.bures@gmail.com>
2018-05-26 11:44:20 +0200

    First working version

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { Alert, FlatList, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { FlatList, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Constants } from 'expo';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      correct: 0,
       countryList: [],
       isLoading: true
     }
@@ -19,37 +21,71 @@ export default class App extends React.Component {
     });
   }
 
-  _onPressButton = () => {
-    Alert.alert('clicked');
+  shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
   }
 
+  getRandomCountry = () => {
+    return this.state.countryList[Math.floor(Math.random() * this.state.countryList.length)];
+  }
+
+  getRandomOptions = (count) => {
+    let options = [];
+    while (options.length < count) {
+      const randomCountry = this.getRandomCountry();
+      if (randomCountry.capital) {
+        options.push({
+          title: randomCountry.capital,
+          correct: false
+        })
+      } else continue;
+    }
+    return options;
+  }
+
+  onPressButton = (e, item) => {
+    if (item.item.correct) {
+      this.setState({
+        correct: this.state.correct + 1
+      });
+    }
+  }
+
+  renderItem = (item) => {
+    return (
+      <TouchableOpacity onPress={e => this.onPressButton(e, item)}>
+        <View style={styles.option}>
+          <Text>{item.item.title}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  }
+
+
+
   render = () => {
+    if (this.state.isLoading) {
+      return <Text>Loading</Text>
+    }
+    const country = this.getRandomCountry();
+    const options = [{
+      title: country.capital,
+      correct: true
+    }].concat(this.getRandomOptions(3));
     return (
       <View style={styles.container}>
-        <TouchableOpacity onPress={this._onPressButton}>
-          <View style={{
-            height: 24,
-            width: 24,
-            borderRadius: 12,
-            borderWidth: 2,
-            borderColor: '#000',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}>
-            <View style={{
-              height: 12,
-              width: 12,
-              borderRadius: 6,
-              backgroundColor: '#000',
-            }}/>
-            <Text>Hello World</Text>
-          </View>
-        </TouchableOpacity>
+        <Text>What is the capital city of</Text>
+        <Text style={styles.titleText}>{country.name}</Text>
         <FlatList
-          data={this.state.countryList}
-          renderItem={item => <Text>{item.item.name}</Text>}
+          data={this.shuffleArray(options)}
+          renderItem={item => this.renderItem(item)}
           keyExtractor={(item, index) => String(index)}
         />
+        <Text>Correct answers: {this.state.correct}</Text>
       </View>
     );
   }
@@ -57,9 +93,20 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#fff',
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    paddingTop: '10%'
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
   },
+  titleText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  option: {
+    borderWidth: 2,
+    borderColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
 });
